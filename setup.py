d51b598dc0958082b96a9f3de8a769f6c463642c
ianb <ianb@localhost>
2006-07-27 16:18:47 +0000

    Added description

--- setup.py
+++ setup.py
@@ -7,12 +7,33 @@ setup(name='wphp',
       version=version,
       description="WSGI-to-PHP connector",
       long_description="""\
+This module allows you to run PHP processes inside of Python, using a
+WSGI gateway.  This way PHP applications can appear like normal Python
+WSGI applications, and WSGI middleware routing and filters can all be
+applied in front of them.  For instance, WSGI middleware-based
+authentication or authorization, routing, deployment, or styling
+filters (like `WSGIOverlay <http://pythonpaste.org/wsgioverlay/>`_).
+
+You can use it like::
+
+    from wphp import PHPApp
+    my_php_app = PHPApp('/path/to/php-files',
+                        php_options={'magic_quote_gpc': 'Off'})
+
+And ``my_php_app`` will be a WSGI application you can embed in other
+Python applications.
 """,
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Paste',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+      ],
       keywords='wsgi php web',
       author='Ian Bicking',
       author_email='ianb@colorstudy.com',
-      url='',
+      url='http://pythonpaste.org/wphp',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
